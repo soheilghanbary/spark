674d65f2b8f9d7812d2b76c3d3787d28fc78c70f
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-05-27 22:26:48 +0330

    add react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { api } from './lib/api'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    async function getUsers() {
+const useUsers = () => {
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
       const res = await api.users.$get()
       const data = await res.json()
-      console.log(data)
+      return data
     }
-    getUsers()
   })
+}
 
+export function App() {
+  const { data, isPending } = useUsers()
+  if (isPending) return <p>Loading Data...</p>
   return (
     <section className='p-4'>
       <h1 className='text-blue-600 font-medium'>Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </section>
   )
 }
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+import './styles/app.css'
+
+import { App } from '@/App'
+import { QueryProvider } from '@/components/providers/query-provider.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles/app.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </React.StrictMode>,
 )
